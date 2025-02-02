271eb5bd97ab9c921c6b7ff84beb02083b34248d
이태윤 <tyleeg0901@gmail.com>
2025-02-02 20:10:34 +0900

    initial commit

---
+++ matching/src/test/java/mataelee/cabsy/matching/matching/MatchingApplicationTests.java
@@ -0,0 +1,13 @@
+package mataelee.cabsy.matching.matching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
